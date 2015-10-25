59ec73caeb19475fdb5c37672d8a2537a9176d57
likangde <likangde@likangdedemacbook-air.local>
2015-10-25 19:35:39 +0800

    Initial Commit

---
+++ MicroblogSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MicroblogSwift
+//
+//  Created by likangde on 15/10/25.
+//  Copyright © 2015年 likangde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
